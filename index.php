aa82be0e87b869b7d1534e74797817ee0452f528
coercion <mark.bridgman@gmail.com>
2013-09-14 19:04:31 -0600

    More beautification

--- index.php
+++ index.php
@@ -1,40 +1,40 @@
 <?php
-require_once('lib/include.php');
-// Detect is the user is logged in, otherwise direct to login page
-if (!isset($_SESSION['user']))
-{
-  header("Location: login.php");
-  exit();
-}
-// Load the user object
-$user = unserialize($_SESSION['user']);
+  require_once('lib/include.php');
+  // Detect is the user is logged in, otherwise direct to login page
+  if (!isset($_SESSION['user']))
+  {
+    header("Location: login.php");
+    exit();
+  }
+  // Load the user object
+  $user = unserialize($_SESSION['user']);
 ?>
 
 <!DOCTYPE HTML>
 <html>
 <head>
   <script type="text/javascript">
-//  var publicKey = "<?php //print $user->pubkey; ?>";
-var name = "<?php print $user->name; ?>";
-var email = "<?php print $user->email; ?>";
-</script>
-<script type="text/javascript" src="jquery.min.js"></script>
-<script type="text/javascript" src="openpgpjs/resources/openpgp.js"></script>
-<script type="text/javascript" src="smail.js"></script>
-<script type="text/javascript">
-function smailOnLoad()
-{
-  $(".enc").each(function(index) {
-    $(this).text = rsaDecrypt(openpgp.keyring.exportPrivateKey, $(this).text());
-  });
-}
-
-</script>
-<link rel="stylesheet" type="text/css" href="smail.css" media="screen" />
-
-<title>SecureMail</title>
+    //  var publicKey = "<?php //print $user->pubkey; ?>";
+    var name = "<?php print $user->name; ?>";
+    var email = "<?php print $user->email; ?>";
+  </script>
+  <script type="text/javascript" src="jquery.min.js"></script>
+  <script type="text/javascript" src="openpgpjs/resources/openpgp.js"></script>
+  <script type="text/javascript" src="smail.js"></script>
+  <script type="text/javascript">
+    function smailOnLoad()
+    {
+      $(".enc").each(function(index) {
+        $(this).text = rsaDecrypt(openpgp.keyring.exportPrivateKey, $(this).text());
+      });
+    }
+  </script>
+  <link rel="stylesheet" type="text/css" href="smail.css" media="screen" />
+  <title>SecureMail</title>
 </head>
+
 <body onload="smailOnLoad()">
+  <!-- body overlay for fancy transparent backgrounds and such -->
   <div id="body_overlay">
     <!-- Header/Logo -->
     <div id="header">
@@ -55,14 +55,14 @@ function smailOnLoad()
       </div>
 
       <div id="content">
-
         <?php require_once("mailbox.php"); ?>
-
       </div>
-    </div>
- <div id="footer"></div>
- <div style="clear:both;"><br/></div>
-</div>
+
+    </div> <!-- body_overlay -->
+
+    <div id="footer"></div>
+    <div style="clear:both;"><br/></div>
+  </div>
 </body>
 
 </html>
