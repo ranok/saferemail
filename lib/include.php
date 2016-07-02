4c0b2746c9543837953ad8eeec3b48e7a3084aba
Jacob Torrey <jacob@jacobtorrey.com>
2016-07-02 19:10:42 -0400

    Cleaning out more stuff from include
    Signed-off-by: Jacob Torrey <jacob@jacobtorrey.com>

--- lib/include.php
+++ lib/include.php
@@ -1,8 +1,6 @@
 <?php
 session_start();
 require_once('config.php');
-require_once('db.class.php');
-//require_once('user.class.php');
 require_once(__DIR__ . '/../vendor/autoload.php');
 require_once(__DIR__ . '/../vendor/generated-conf/config.php');
 ?>
